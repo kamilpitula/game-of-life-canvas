33bf99454155f5a3e897d8f0770f006f67df0b3c
Kamil Pitula <Kamil_Pitula@epam.com>
2024-02-02 14:48:14 +0100

    improve buttons styling

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,34 @@ import { createScene, runScene, stopScene } from "./gameScene";
 import { Game } from "./model/Game";
 import "./style.css";
 
+function setButtonState(
+  button: HTMLButtonElement,
+  state: "active" | "disabled"
+) {
+  if (state === "disabled") {
+    button.classList.add("bg-slate-400");
+    button.classList.add("hover:bg-slate-500");
+    return;
+  }
+
+  button.classList.remove("bg-slate-400");
+  button.classList.remove("hover:bg-slate-500");
+}
+
 const startBtn = <HTMLButtonElement>document.getElementById("startBtn");
 const stopBtn = <HTMLButtonElement>document.getElementById("stopBtn");
+  setButtonState(stopBtn, "disabled");
 
-startBtn.addEventListener("click", runScene);
-stopBtn.addEventListener("click", stopScene);
+startBtn.addEventListener("click", () => {
+  runScene();
+  setButtonState(startBtn, "disabled");
+  setButtonState(stopBtn, "active");
+});
+stopBtn.addEventListener("click", () => {
+  stopScene();
+  setButtonState(startBtn, "active");
+  setButtonState(stopBtn, "disabled");
+});
 
 const height = 100;
 const width = 100;
